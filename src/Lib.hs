94313c6dde26b5c9d56675950a06b590cccd55d9
Gorev-V <vasilygorev@yandex.ru>
2017-11-25 21:55:03 +0300

    removed usless comments

--- src/Lib.hs
+++ src/Lib.hs
@@ -252,54 +252,12 @@ updAucJson cfg = do
     unless b $ do
         ti <- trackingItems
         harvestAuctionJson cfg ti a r
-        --changeUpdTime (updatedAt cfg) s t
-
-
 
 
 myfun :: IO ()
 myfun = do
-    {--
-    conf <- load [Required "./config.cfg"]
-    let dbconf = subconfig "database" conf
-    let apiconf = subconfig "api" conf
-    connectUser <- require dbconf "username"
-    connectPassword <- require dbconf "password"
-    connectDatabase <- require dbconf "database"
-    connectHost <- require dbconf "host"
-    connectPort <- require dbconf "port"
-    apiKey <- require apiconf "key" :: IO ApiKey
-    apiLimit <- require apiconf "limit"
-    region <- read <$> require apiconf "region" :: IO Region
-    langLocale <- read <$> require apiconf "langLocale" :: IO Locale
-    filterLocale <- fmap read <$> require apiconf "filterLocale" :: IO [Locale]
-    reqQueue <- newMVar S.empty :: IO (MVar (S.Seq ReqParams))
-    dlChan <- atomically newTChan :: IO (TChan DLParams)
-    counter <- newMVar apiLimit :: IO (MVar Int)
-    updatedAt <- newMVar M.empty :: IO (MVar (M.Map Slug UTCTime))
-    manager <- C.newManager C.tlsManagerSettings
-    let connInfo = ConnectInfo { connectHost
-                               , connectPort
-                               , connectUser
-                               , connectPassword
-                               , connectDatabase
-                               }
-    connPool <- createPool (connect connInfo) close 1 10 20
-    let cfg = Config { apiKey
-                     , region
-                     , langLocale
-                     , filterLocale
-                     , counter
-                     , reqQueue
-                     , manager
-                     , dlChan
-                     , updatedAt
-                     , connPool
-                     } --}
     cfg <- readCfg "./config.cfg"
     withResource (connPool cfg) initMigrations
-    --conn <- connect connInfo
-    --initMigrations conn
     forkIO $ forever $ updAucJson cfg
     forkIO $ forever $ do 
         addReqToQ cfg (ReqRealms cfg)
