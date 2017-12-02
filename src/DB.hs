e6e76b5f3cf04e779e3daa986b8b1760a8960c0a
Gorev-V <vasilygorev@yandex.ru>
2017-12-02 15:21:43 +0300

    deleted usless function

--- src/DB.hs
+++ src/DB.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module DB (initMigrations, writeBoxInTBuyout , writeBoxInDB, updLastModified)
+module DB (initMigrations, writeBoxInDB, updLastModified)
     where
 
 import Database.PostgreSQL.Simple
@@ -42,11 +42,7 @@ sqlHandlers = [Handler (\ (ex::SqlError) -> hSqlErr ex)    ,
                Handler (\ (ex::QueryError) -> hSqlErr ex)  ,
                Handler (\ (ex::FormatError) -> hSqlErr ex) ]
 
-writeBoxInTBuyout :: UTCTime -> Slug -> Int -> WBox -> Pool Connection -> IO Int64
-writeBoxInTBuyout date slug' iid' box connPool' = withResource connPool' execute' `catches` sqlHandlers
-        where qdata =  (date,  slug' , iid') :. box
-              q = "insert into buyout (buyout_date, server_slug, item_id, item_count, bot_w, p_25, p_50, p_75, top_w) values (?,?,?,?,?,?,?,?,?)"
-              execute' conn = execute conn q qdata
+
 
 writeBoxInDB :: UTCTime -> Slug -> [(Int, WBoxedStats)] -> Pool Connection -> IO Int64
 writeBoxInDB date slug' kv connPool' = do 
