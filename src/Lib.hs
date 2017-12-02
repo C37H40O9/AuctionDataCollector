2abb092db5e0babee5afec65e8ecabafc699f2cb
Gorev-V <vasilygorev@yandex.ru>
2017-12-02 20:17:28 +0300

    indents

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,8 +5,7 @@
 --{-# LANGUAGE TransformListComp #-}
 --{-# LANGUAGE OverloadedRecordFields #-}
 
-module Lib
-     where
+module Lib where
 import Types.Types
 import Types.Locale
 import DB
@@ -37,10 +36,10 @@ import Control.Exception
 
 trackingItems :: IO TrackingItems
 trackingItems = do
-    res <- itemsDec
-    case res of
-        Left err -> pure []
-        Right i -> pure $ map iid (items i)
+  res <- itemsDec
+  case res of
+    Left err -> pure []
+    Right i -> pure $ map iid (items i)
 
 
 parseToBox :: [Int] -> WBox
@@ -50,13 +49,13 @@ parseToBox [i, a, b, c, d, e, f, g] = WBox i a b c d e f g
 seqToBox :: S.Seq Int -> Maybe WBox
 seqToBox s | S.null s = Nothing
            | otherwise = Just $ parseToBox  $ l : ws
-                where
-                    l = S.length s
-                    maxIndex = l - 1
-                    s' = S.sort s
-                    ix :: [Int]
-                    ix = quot . (* maxIndex) <$> [0,9,25,50,75,91,100] <*> pure 100
-                    ws = S.index s' <$> ix
+           where
+             l = S.length s
+             maxIndex = l - 1
+             s' = S.sort s
+             ix :: [Int]
+             ix = quot . (* maxIndex) <$> [0,9,25,50,75,91,100] <*> pure 100
+             ws = S.index s' <$> ix
                    
 seqStatsToWBoxed :: IStats -> WBoxedStats
 seqStatsToWBoxed s = WBoxedStats (seqToBox $ bid' s) (seqToBox $ buyout' s)
@@ -102,43 +101,44 @@ parseRealms x = parseMaybe realmsParser =<< decode x
 
 
 aucToIStats :: Auction -> IStats
-aucToIStats a = IStats { bid'    = S.replicate (quantity a) (quot  (bid a)    (quantity a))
-                       , buyout' = if buyout a > 0 then S.replicate (quantity a) (quot  (buyout a) (quantity a))  else S.empty
+aucToIStats a = IStats {bid' = S.replicate (quantity a) (quot  (bid a) (quantity a))
+                       ,buyout' = if buyout a > 0 then S.replicate (quantity a) (quot (buyout a) (quantity a)) else S.empty
                        }
 
 statsConcat :: IStats -> IStats -> IStats
-statsConcat  s1 s2 = IStats bi bu
-    where bi = bid'    s1 S.>< bid' s2
-          bu = buyout' s1 S.>< buyout' s2
+statsConcat s1 s2 = IStats bi bu
+  where
+    bi = bid' s1 S.>< bid' s2
+    bu = buyout' s1 S.>< buyout' s2
           
 
 --foldl' :: Foldable t => (b -> a -> b) -> b -> t a -> b
 
-collect ::  [Auction] -> M.Map Int IStats
-collect  = foldl' (\b a -> M.insertWith statsConcat (itemId a) (aucToIStats a) b ) M.empty 
+collect :: [Auction] -> M.Map Int IStats
+collect = foldl' (\b a -> M.insertWith statsConcat (itemId a) (aucToIStats a) b ) M.empty
 
-allHttpExHandler ::  SomeException -> IO B.ByteString
+allHttpExHandler :: SomeException -> IO B.ByteString
 allHttpExHandler e = print e *> pure B.empty
 
 updateRealms :: Config -> IO ()
 updateRealms cfg = do
-    req <- C.parseRequest $  "https://" <> show (region cfg) <> ".api.battle.net/wow/realm/status?locale=" <> show (langLocale cfg) <> "&apikey=" <> apiKey cfg
-    let res = runResourceT $ do               
-                response <- C.httpLbs (setRequestIgnoreStatus req) $ manager cfg
-                pure $  C.responseBody response
-    rj <- res `catch` allHttpExHandler
-    incrCounter $ counter cfg
-    case parseRealms rj of 
-        Nothing -> pure ()
-        Just x -> addReqsToQ cfg $ S.fromList $ map (ReqAuc cfg ) $ filterRealmsByLocale (filterLocale cfg) $ filterSameRealms x
+  req <- C.parseRequest $ "https://" <> show (region cfg) <> ".api.battle.net/wow/realm/status?locale=" <> show (langLocale cfg) <> "&apikey=" <> apiKey cfg
+  let res = runResourceT $ do
+    response <- C.httpLbs (setRequestIgnoreStatus req) $ manager cfg
+    pure $ C.responseBody response
+  rj <- res `catch` allHttpExHandler
+  incrCounter $ counter cfg
+  case parseRealms rj of 
+    Nothing -> pure ()
+    Just x -> addReqsToQ cfg $ S.fromList $ map (ReqAuc cfg ) $ filterRealmsByLocale (filterLocale cfg) $ filterSameRealms x
     
 filterItems :: TrackingItems -> ([Auction] -> [Auction])
-filterItems ti  = filter (\i -> itemId i `elem` ti) 
+filterItems ti  = filter (\i -> itemId i `elem` ti)
 
 filterSameRealms :: [Realm] -> [Realm]
 filterSameRealms [] = []
 filterSameRealms (x:xs) = x : t
-    where t = filterSameRealms $ filter (\y -> slug x `notElem` connectedRealms y ) xs
+  where t = filterSameRealms $ filter (\y -> slug x `notElem` connectedRealms y ) xs
 
 filterRealmsByLocale :: [Locale] -> [Realm] -> [Realm]
 filterRealmsByLocale _ [] = []
@@ -147,121 +147,120 @@ filterRealmsByLocale ls rs = filter (\y -> locale y `elem` ls) rs
 
 takeAuctionInfo :: Config -> Realm -> IO ()
 takeAuctionInfo cfg r = do
-    req <- C.parseRequest $  "https://" <> show (region cfg) <> ".api.battle.net/wow/auction/data/" <> slug r <> "?locale=" <> show (langLocale cfg) <> "&apikey=" <> apiKey cfg
-    let res = runResourceT $ do            
-                response <- C.httpLbs  (setRequestIgnoreStatus req) $ manager cfg
-                pure $ C.responseBody response
-    aj <- res `catch` allHttpExHandler
-    incrCounter $ counter cfg
-    case parseAucFile aj of
-        Nothing -> pure ()
-        Just x ->  atomically $ writeTChan (dlChan cfg) (DLAucJson x r)
-    
+  req <- C.parseRequest $  "https://" <> show (region cfg) <> ".api.battle.net/wow/auction/data/" <> slug r <> "?locale=" <> show (langLocale cfg) <> "&apikey=" <> apiKey cfg
+  let res = runResourceT $ do
+    response <- C.httpLbs  (setRequestIgnoreStatus req) $ manager cfg
+    pure $ C.responseBody response
+  aj <- res `catch` allHttpExHandler
+  incrCounter $ counter cfg
+  case parseAucFile aj of
+    Nothing -> pure ()
+    Just x ->  atomically $ writeTChan (dlChan cfg) (DLAucJson x r)
 
 
-harvestAuctionJson :: Config -> TrackingItems -> AucFile -> Realm ->  IO ()
-harvestAuctionJson cfg ti a r = do    
-    let t = millisToUTC $ lastModified a
-        s = slug r
-        connP = connPool cfg
-    putStrLn $ rname r <> " @ " <> show t
-    req <- C.parseRequest $ url a
-    aj<-runResourceT $ do 
-            response <- C.httpLbs (setRequestIgnoreStatus req) $ manager cfg
-            pure $ C.responseBody response
-    case parseAuctions aj of
-        Nothing -> pure ()
-        Just x -> do
-            let l = M.toList $ M.map seqStatsToWBoxed $ collect $ filterItems ti x
-            i <- writeBoxInDB t s l connP
-            print i
-            if i > 0 then do
-                ui <- updLastModified t s connP
-                print ui
-                changeUpdTime (updatedAt cfg) s t
-            else pure ()
+harvestAuctionJson :: Config -> TrackingItems -> AucFile -> Realm -> IO ()
+harvestAuctionJson cfg ti a r = do
+  let t = millisToUTC $ lastModified a
+    s = slug r
+    connP = connPool cfg
+  putStrLn $ rname r <> " @ " <> show t
+  req <- C.parseRequest $ url a
+  aj<-runResourceT $ do 
+    response <- C.httpLbs (setRequestIgnoreStatus req) $ manager cfg
+    pure $ C.responseBody response
+  case parseAuctions aj of
+    Nothing -> pure ()
+    Just x -> do
+      let l = M.toList $ M.map seqStatsToWBoxed $ collect $ filterItems ti x
+      i <- writeBoxInDB t s l connP
+      print i
+      if i > 0 then do
+        ui <- updLastModified t s connP
+        print ui
+        changeUpdTime (updatedAt cfg) s t
+      else pure ()
 
 
 addReqToQ :: Config -> ReqParams -> IO ()
 addReqToQ cfg reqParam = do
-    rq' <- takeMVar (reqQueue cfg)
-    putMVar (reqQueue cfg) $ rq' S.|> reqParam
+  rq' <- takeMVar (reqQueue cfg)
+  putMVar (reqQueue cfg) $ rq' S.|> reqParam
 
 addReqsToQ :: Config -> S.Seq ReqParams  -> IO ()
 addReqsToQ cfg reqParams = do
-    rq' <- takeMVar (reqQueue cfg)
-    putMVar (reqQueue cfg) $ rq' S.>< reqParams
+  rq' <- takeMVar (reqQueue cfg)
+  putMVar (reqQueue cfg) $ rq' S.>< reqParams
 
 
 incrCounter :: MVar Int -> IO ()
 incrCounter counter = do
-    c <- takeMVar counter
-    putMVar counter $ c + 1
+  c <- takeMVar counter
+  putMVar counter $ c + 1
 
 millisToUTC :: Integer -> UTCTime
 millisToUTC t = posixSecondsToUTCTime $ fromInteger t / 1000
 
 runRequest :: ReqParams  -> IO()
 runRequest rp = case rp of
-    ReqAuc cfg r    -> takeAuctionInfo cfg r
-    ReqRealms cfg   -> updateRealms cfg
+  ReqAuc cfg r -> takeAuctionInfo cfg r
+  ReqRealms cfg -> updateRealms cfg
 
 
 runJob :: Config -> IO ()
 runJob cfg = do
-    c' <- takeMVar (counter cfg)
-    rq' <- takeMVar (reqQueue cfg)
-    let rqlen = S.length rq'
-    if rqlen >= c'
-        then do
-            putMVar (counter cfg) 0
-            let (r,t) = S.splitAt c' rq'
-            putMVar (reqQueue cfg) t
-            mapConcurrently_ runRequest r 
-        else do
-            putMVar (counter cfg) (c' - rqlen)
-            putMVar (reqQueue cfg) S.empty
-            mapConcurrently_ runRequest rq'
+  c' <- takeMVar (counter cfg)
+  rq' <- takeMVar (reqQueue cfg)
+  let rqlen = S.length rq'
+  if rqlen >= c'
+    then do
+      putMVar (counter cfg) 0
+      let (r,t) = S.splitAt c' rq'
+      putMVar (reqQueue cfg) t
+      mapConcurrently_ runRequest r 
+    else do
+      putMVar (counter cfg) (c' - rqlen)
+      putMVar (reqQueue cfg) S.empty
+      mapConcurrently_ runRequest rq'
 
 isActual :: MVar (M.Map Slug UTCTime) -> Slug -> UTCTime  -> IO Bool
 isActual m s t = do
-    m' <- readMVar m
-    let v = M.lookup s m'
-    case v of
-        Nothing -> pure False
-        Just x -> pure $ x >= t
+  m' <- readMVar m
+  let v = M.lookup s m'
+  case v of
+    Nothing -> pure False
+    Just x -> pure $ x >= t
 
 changeUpdTime :: MVar (M.Map Slug UTCTime) -> Slug -> UTCTime  -> IO ()
 changeUpdTime u s t = do
-    u' <- takeMVar u
-    putMVar u $ M.insert s t u'
+  u' <- takeMVar u
+  putMVar u $ M.insert s t u'
 
 
 updAucJson ::  Config -> IO () 
 updAucJson cfg = do
-    DLAucJson a r <- atomically $ readTChan (dlChan cfg)
-    let t = millisToUTC $ lastModified a 
-        s = slug r
-    b <- isActual (updatedAt cfg) s t
-    unless b $ do
-        ti <- trackingItems
-        harvestAuctionJson cfg ti a r
+  DLAucJson a r <- atomically $ readTChan (dlChan cfg)
+  let t = millisToUTC $ lastModified a 
+      s = slug r
+  b <- isActual (updatedAt cfg) s t
+  unless b $ do
+    ti <- trackingItems
+    harvestAuctionJson cfg ti a r
 
 loadLastModified :: Config -> IO ()
 loadLastModified cfg = do
-    xs <- readLastModified (connPool cfg)
-    putMVar (updatedAt cfg) $ M.fromList xs
+  xs <- readLastModified (connPool cfg)
+  putMVar (updatedAt cfg) $ M.fromList xs
 
 myfun :: IO ()
 myfun = do
-    cfg <- readCfg "./config.cfg"
-    withResource (connPool cfg) initMigrations
-    loadLastModified cfg
-    forkIO $ forever $ updAucJson cfg
-    forkIO $ forever $ do 
-        addReqToQ cfg (ReqRealms cfg)
-        threadDelay $ 120 * oneSecond
-    forever $ do
-                forkIO $ runJob cfg
-                threadDelay oneSecond
-                pure ()
+  cfg <- readCfg "./config.cfg"
+  withResource (connPool cfg) initMigrations
+  loadLastModified cfg
+  forkIO $ forever $ updAucJson cfg
+  forkIO $ forever $ do 
+    addReqToQ cfg (ReqRealms cfg)
+    threadDelay $ 120 * oneSecond
+  forever $ do
+    forkIO $ runJob cfg
+    threadDelay oneSecond
+    pure ()
